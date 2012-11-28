81c0b29f43946197721b424e7e4bb33a819247bd
flags <jetstarforever@gmail.com>
2012-11-28 12:11:53 -0500

    Quotes.

--- compile_cython_modules.py
+++ compile_cython_modules.py
@@ -4,5 +4,5 @@ from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("render_map", ["render_map.pyx"])]
+    ext_modules = [Extension('render_map', ['render_map.pyx'])]
 )
