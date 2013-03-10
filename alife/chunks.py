7b0b41029bf3e44336bc1fd0a11c11ced58b5785
flags <jetstarforever@gmail.com>
2013-03-10 03:01:39 -0400

    fixed traveling chunks

--- alife/chunks.py
+++ alife/chunks.py
@@ -39,36 +39,38 @@ def find_best_unknown_chunk(life, chunks):
 	_best_chunk = {'distance': 99999, 'chunk_keys': []}
 	_building_found = False
 	for chunk_key in chunks:
-		_chunk_pos = [int(value) for value in chunk_key.split(',')]
-		_chunk_center = (_chunk_pos[0]+SETTINGS['chunk size'], _chunk_pos[1]+SETTINGS['chunk size'])
-		_distance = numbers.distance(life['pos'], _chunk_center)
+		pass
+		#_chunk_pos = [int(value) for value in chunk_key.split(',')]
+		#_chunk_center = (_chunk_pos[0]+SETTINGS['chunk size'], _chunk_pos[1]+SETTINGS['chunk size'])
+		#_distance = numbers.distance(life['pos'], _chunk_center)
 		
-		if not can_see_chunk(life, chunk_key):
-			print 'Cant see yah, boss'
+		#if not can_see_chunk(life, chunk_key):
+		#	print 'Cant see yah, boss'
+		#	continue
+		
+		#if maps.get_chunk(chunk_key)['type'] in ['building', 'road']:
+		#	if not _building_found:
+		#		_best_chunk['chunk_keys'] = []
+		#		_building_found = True
+			
+		#	_best_chunk['chunk_keys'].append(chunk_key)
+		
+		#if not _building_found:
+		#	_best_chunk['chunk_keys'].append(chunk_key)
+	
+	_nearest = {'distance': -1, 'key': None}
+	for chunk_key in find_nearest_road(life):
+		if chunk_key in life['known_chunks']:
 			continue
 		
-		if maps.get_chunk(chunk_key)['type'] in ['building', 'road']:
-			if not _building_found:
-				_best_chunk['chunk_keys'] = []
-				_building_found = True
-			
-			_best_chunk['chunk_keys'].append(chunk_key)
+		chunk_center = [int(val)+SETTINGS['chunk size'] for val in chunk_key.split(',')]
+		_distance = numbers.distance(life['pos'], chunk_center)
 		
-		if not _building_found:
-			_best_chunk['chunk_keys'].append(chunk_key)
-		
-		#if _distance<=_best_chunk['distance']:
-		#	_best_chunk['distance'] = _distance
-			
-		#	if _distance == _best_chunk['distance']:
-		#_best_chunk['chunk_keys'].append(chunk_key)
-		#	else:
-		#		_best_chunk['chunk_keys'] = [chunk_key]
-		
-	if not _best_chunk['chunk_keys']:
-		return False
+		if not _nearest['key'] or _distance<_nearest['distance']:
+			_nearest['distance'] = _distance
+			_nearest['key'] = chunk_key
 	
-	return random.choice(_best_chunk['chunk_keys'])
+	return _nearest['key']
 
 def find_unknown_chunks(life):
 	_unknown_chunks = []
@@ -78,6 +80,25 @@ def find_unknown_chunks(life):
 			_unknown_chunks.append(chunk_id)
 	
 	return _unknown_chunks
+
+def _find_nearest_reference(life, type):
+	_lowest = {'return': None, 'distance': -1}
+	for entry in REFERENCE_MAP[type]:
+		for _pos in entry:
+			pos = [int(val)+SETTINGS['chunk size'] for val in _pos.split(',')]
+			_distance = numbers.distance(life['pos'], pos)
+			
+			if not _lowest['return'] or _distance<_lowest['distance']:
+				_lowest['distance'] = _distance
+				_lowest['return'] = entry
+	
+	return _lowest['return']
+
+def find_nearest_road(life):
+	return _find_nearest_reference(life, 'roads')
+
+def find_nearest_building(life):
+	return _find_nearest_reference(life, 'buildings')
 
 def is_in_chunk(life, chunk_id):
 	_chunk = maps.get_chunk(chunk_id)
