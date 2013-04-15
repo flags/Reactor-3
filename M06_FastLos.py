a2dbcf199a80c673ba2ba6d9aba29c8cc0ccbc07
flags <jetstarforever@gmail.com>
2013-04-15 16:39:50 -0400

    Fast los work

--- M06_FastLos.py
+++ M06_FastLos.py
@@ -8,18 +8,18 @@ sight = 30
 start_point = (45, 45)
 source_map = numpy.zeros((100, 100))
 source_map[30,31] = 1
+source_map[46,46] = 1
 los = numpy.zeros((30, 30))
 
 #At 45 degrees we are sending out 8 feelers
 #If they don't find anything then we'll scale down
 
-def check_dirs(intensity=45, already_checked={}):
+def check_dirs(intensity=45, already_checked={}, scan=(0, 360), quad_check=True):
 	_check_dirs = already_checked
-	#print 'Vals',already_checked
 	_checked_quads = [deg/90 for deg in already_checked]
 	
-	for deg in range(0, 360, intensity):
-		if deg/90 in _checked_quads:
+	for deg in range(scan[0], scan[1], intensity):
+		if quad_check and deg/90 in _checked_quads:
 			continue
 		
 		_end_x,_end_y = numbers.velocity(deg, sight)[:2]
@@ -28,28 +28,55 @@ def check_dirs(intensity=45, already_checked={}):
 		_i = 0
 		for pos in _line:
 			_i += 1
+			los[pos[1]-start_point[1], pos[0]-start_point[0]] = 1
 			if source_map[pos[1], pos[0]]:
 				_check_dirs[deg] = _line[:_i]
 				break
 	
-	#print check_dirs
 	return _check_dirs
 
-#if check_
 _check_dirs = {}
 intensity = 45
 
 while 1:
 	_check_dirs = check_dirs(intensity=intensity, already_checked=_check_dirs)
-	quads_to_check = [entry for entry in _check_dirs if _check_dirs[entry]]
+	quads_to_check = [entry/90 for entry in _check_dirs if _check_dirs[entry]]
 	intensity /= 2
 	
-	if quads_to_check:
+	if intensity<=8:
 		break
 
-print quads_to_check
+for quad in quads_to_check:
+	_scan = scan=(numbers.clip(quad*90, 0, 360), (numbers.clip((quad+1)*90, 0, 360)))
+	#print _scan
+	_check_dirs.update(check_dirs(intensity=1, scan=_scan, quad_check=False))
+
+#for line in [_check_dirs[entry] for entry in _check_dirs if _check_dirs[entry]]:
+#	for pos in line:
+#		#print pos
+#		x,y = pos
+#		x-=start_point[0]
+#		y-=start_point[1]
+#		
+#		los[y,x] = 1
+#		#print x, y
 
 print time.time()-_stime
+print _check_dirs.keys()
+print quads_to_check
+
+for _y in range(30):
+	for _x in range(30):
+		#x = (start_point[0]-_x)
+		#y = _y-(start_point[1]-15)
+		#print x,y
+		if (_x,_y) == start_point:
+			print '@',
+		elif los[_y, _x]:
+			print int(los[_y, _x]),
+		else:
+			print '',
+	print
 
 #for entry in to_check:
 #	print entry
