bfa356be88ab0d0ab7ee3377ac0fb13c698a78f2
flags <jetstarforever@gmail.com>
2013-05-08 16:15:40 -0400

    ALife find and share camp founder info with others

--- alife/alife_camp.py
+++ alife/alife_camp.py
@@ -2,13 +2,13 @@ from globals import *
 
 import life as lfe
 
-import brain
-
 import references
 import judgement
 import movement
 import speech
+import brain
 import camps
+import jobs
 import maps
 
 import logging
@@ -53,8 +53,6 @@ def tick(life, alife_seen, alife_not_seen, targets_seen, targets_not_seen, sourc
 	#NOTE: We've already been attracted here. Think twice before abandoning...
 	_info = camps.get_camp_info(life, _camp)
 	
-	#print _camp['id'],[camp['id'] for camp in camps.get_founded_camps(life)]
-	
 	if not _info['founder']==-1:
 		if not camps.is_in_camp(life, _camp):
 			_closest_key =  references.find_nearest_key_in_reference(life, _camp['reference'])
@@ -65,16 +63,44 @@ def tick(life, alife_seen, alife_not_seen, targets_seen, targets_not_seen, sourc
 				'to': random.choice(_chunk['ground'])},
 				200)
 			return False
-		#print 'can help!',_info['founder']
-		#print lfe.get_memory(life, matches={'text': 'help find founder'})
-		#print life['name'],_camp['id'],lfe.get_memory(life, matches={'text': 'help find founder'})
+		
+		_can_actually_help = []
 		for can_help_find in lfe.get_memory(life, matches={'camp': _camp['id'], 'text': 'help find founder'}):
-			print can_help_find['target']
+			#TODO: Haven't implemented `knows about founder`
+			if not lfe.get_memory(life, matches={'target': can_help_find['target'], 'camp': _camp['id'], 'text': 'knows about founder'}):
+				_can_actually_help.append(can_help_find['target'])
+		
+		#TODO: Score these
+		if _can_actually_help:
+			#This should always return something
+			_target = brain.knows_alife_by_id(life, _can_actually_help.pop())
+			
+			if not lfe.get_memory(life, matches={'target': can_help_find['target'], 'camp': _camp['id'], 'text': 'go tell about founder'}):
+				lfe.memory(life, 'go tell about founder',
+					camp=_camp['id'],
+					target=_target['life']['id'])
+					
+				#This will be a very general job that give another alife a specific memory
+				_j = jobs.create_job(life, 'give information')
+				jobs.add_detail_to_job(_j, 'say', {'gist': 'camp_founder',
+					'founder': _info['founder'],
+					'camp': _camp['id'],
+					'target': _target['life']['id']})
+				jobs.add_detail_to_job(_j, 'target', _target['life']['id'])
+				jobs.add_job_task(_j, 'find alife', callback=movement.find_alife_and_say, required=True)
+				jobs.add_job_candidate(_j, life)
+				jobs.process_job(_j)
+			else:
+				print 'Already done...'
+			
+			#lfe.clear_actions(life)
+			#lfe.add_action(life,{'action': 'move',
+			#	'to': _target['last_seen_at'][:2]},
+			#	200)
 	else:		
 		#Try to find out who he is...
 		speech.announce(life, 'who_is_founder', camp=_camp['id'])
 		
-		#if lfe.ticker(life, 'who_is_founder', 100):
 		_possible_people_to_ask = []
 		for target in speech.get_announce_list(life):
 			#TODO: In this case we'll be approached (maybe) by the ALife if they find the info...
@@ -97,6 +123,7 @@ def tick(life, alife_seen, alife_not_seen, targets_seen, targets_not_seen, sourc
 					_found = True
 					break					
 			
+			#TODO: Job needed here
 			if _found:
 				lfe.clear_actions(life)
 				lfe.add_action(life,{'action': 'move',
