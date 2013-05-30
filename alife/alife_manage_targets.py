9bd074bc6e166337d25eaead4434e0443bf3fd81
flags <jetstarforever@gmail.com>
2013-05-30 03:11:13 -0400

    ALife module for managing targets

---
+++ alife/alife_manage_targets.py
@@ -0,0 +1,26 @@
+from globals import *
+import life as lfe
+
+import alife
+import raids
+
+def conditions(life, alife_seen, alife_not_seen, targets_seen, targets_not_seen, source_map):
+	return RETURN_SKIP
+
+def add_raid_targets(life):
+	for raider in [LIFE[r] for r in raids.get_raiders(life['camp'])]:
+		if not alife.brain.knows_alife(life, raider):
+			alife.brain.meet_alife(life, raider)
+		
+		if not lfe.get_memory(life, matches={'text': 'target_is_raiding', 'target': raider['id'], 'camp': life['camp']}):
+			lfe.memory(life,
+				'target_is_raiding',
+				target=raider['id'],
+				camp=life['camp'],
+				danger=2,
+			    trust=-10)
+
+def tick(life, alife_seen, alife_not_seen, targets_seen, targets_not_seen, source_map):
+	if life['camp']:
+		if raids.camp_has_raid(life['camp']):
+			add_raid_targets(life)
