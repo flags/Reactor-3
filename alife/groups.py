68de0775680f32eb01e7d69f60fb6a1ae51b1aa7
flags <jetstarforever@gmail.com>
2013-06-22 17:28:53 -0400

    Groups

--- alife/groups.py
+++ alife/groups.py
@@ -11,7 +11,13 @@ def create_group(life):
 	GROUPS[SETTINGS['groupid']] = _group
 	
 	logging.debug('%s created group: %s' % (' '.join(life['name']), SETTINGS['groupid']-1))
+
+def add_member(group_id, life_id):
+	if is_member(group_id, life_id):
+		raise Exception('%s is already a member of group: %s' % (' '.join(LIFE[life_id]), group_id))
 	
+	get_group(group_id)['members'].append(life_id)
+
 def get_group(group_id):
 	if not group_id in GROUPS:
 		raise Exception('Group does not exist: %s' % group_id)
@@ -19,12 +25,19 @@ def get_group(group_id):
 	return GROUPS[group_id]
 
 def is_member(group_id, life_id):
+	_group = get_group(group_id)
 	
+	if life_id in _group['members']:
+		return True
+	
+	return False
 
 def remove_member(group_id, life_id):
 	_group = get_group(group_id)
 	
-	if 
-
-def delete_group(life):
-	for 
+	if not is_member(group_id, life_id):
+		raise Exception('%s is not a member of group: %s' % (' '.join(LIFE[life_id]), group_id))
+	
+def delete_group(group_id):
+	for member in get_group(group_id)['members']:
+		pass
