415eeb92dd8f7d732bbd662e922dd1b28a266e84
flags <jetstarforever@gmail.com>
2013-07-11 16:34:16 -0400

    History gen messages

--- historygen.py
+++ historygen.py
@@ -9,6 +9,7 @@ _HISTORY = {'life': {},
             'camps': {}}
 
 def create_background(life):
+	HISTORY = []
 	#This will decide who the person is and what their deal is...
 	#i.e., we already roll their stats, but this is building up some
 	#credibility behind all that (via implanting memories.)
@@ -21,17 +22,14 @@ def create_background(life):
 	#Just going to start listing things that I believe play a role in
 	#someone's development. Don't take these too seriously...
 	
-	#1 - 6 (POOR - SPOILED)
-	CHILDHOOD_QUALITY = numbers.roll(2, 3)
-	
-	#1 - 6 (ABSENT - STRICT)
-	PARENTING_QUALITY = numbers.roll(2, 3)
-	
 	#BOOL (HAS BIRTH PARENTS)
 	HAS_BIRTH_PARENTS = (random.randint(0, 1) == 1)
 	
 	#BOOL (WAS ADOPTED)
 	WAS_ADOPTED = ((random.randint(0, 1) * (not HAS_BIRTH_PARENTS)) == 1)
+	
+	#1 - 6 (ABSENT - STRICT)
+	PARENTING_QUALITY = numbers.roll(2, 3) * (HAS_BIRTH_PARENTS or WAS_ADOPTED)
 	
 	#BOOL (WAS BULLIED)
 	BULLIED = (random.randint(0, 1) == 1)
@@ -40,7 +38,7 @@ def create_background(life):
 	FOUGHT_BACK = (random.randint(0, 1) == 1)
 	
 	#1 - 10 (DROP OUT < 5, GRADUATED)
-	GRADUATED = (numbers.roll(HAS_BIRTH_PARENTS+WAS_ADOPTED, 5)<5)
+	GRADUATED = (numbers.roll(HAS_BIRTH_PARENTS+WAS_ADOPTED, 5)>=5)
 	
 	#BOOL (STREET SMART)
 	STREET_SMART = ((random.randint(0, 1) * (not GRADUATED)) == 1)
@@ -64,12 +62,11 @@ def create_background(life):
 	MELEE = numbers.roll(FOUGHT_BACK+STREET_SMART, 5)
 	
 	#1 - 10 (CLUELESS - WALL STREET)
-	TRADING = numbers.roll(MAX_STREET_SMARTS+MAX_BOOK_SMART, 2)
+	TRADING = numbers.roll((MAX_STREET_SMARTS+MAX_BOOK_SMART) - (MATHEMATICS<5), 2)
 	
-	print 'Childhood quality:', CHILDHOOD_QUALITY
-	print 'Parenting quality:', PARENTING_QUALITY
 	print 'Birth parents:', HAS_BIRTH_PARENTS
 	print 'Adopted:', WAS_ADOPTED
+	print 'Parenting quality:', PARENTING_QUALITY
 	print 'Bullied:', BULLIED
 	print 'Fought back:', FOUGHT_BACK
 	print 'Graduated:', GRADUATED
@@ -78,8 +75,57 @@ def create_background(life):
 	print 'Reading:', READING
 	print 'Engineering:', ENGINEERING
 	print 'Firearms:', FIREARMS
-	print 'Melee:', MELEE
+	print 'Melee:', MELEE,FOUGHT_BACK+STREET_SMART
 	print 'Trading:', TRADING
+	
+	if not HAS_BIRTH_PARENTS:
+		HISTORY.append('lost his parents in late childhood')
+	
+	if WAS_ADOPTED:
+		HISTORY.append('and was later adopted')
+	
+	if PARENTING_QUALITY>=3:
+		HISTORY.append('Was raised well')
+	elif PARENTING_QUALITY:
+		HISTORY.append('Was raised poorly')
+	
+	if BULLIED:
+		HISTORY.append('Was bullied')
+		
+		if FOUGHT_BACK:
+			HISTORY[len(HISTORY)-1] += ', but he fought back'
+	
+	if GRADUATED:
+		HISTORY.append('Graduated')
+	elif STREET_SMART:
+		HISTORY.append('Is street smart')
+		
+	if MATHEMATICS>=5:
+		HISTORY.append('Is good at math')
+	elif MATHEMATICS <= 2:
+		HISTORY.append('Is bad with numbers')
+	
+	if READING>=5:
+		HISTORY.append('Can read very well')
+	elif READING <= 3:
+		HISTORY.append('Was not hooked on phonics')
+	
+	if ENGINEERING>=5:
+		HISTORY.append('Is proficient at engineering')
+	elif ENGINEERING <= 3:
+		HISTORY.append('Has poor engineering skills')
+	
+	if FIREARMS>=5:
+		HISTORY.append('Knows his way around a gun')
+	elif FIREARMS <= 3:
+		HISTORY.append('Has shaky aim')
+		
+	if MELEE>=5:
+		HISTORY.append('Can fight very well')
+	elif MELEE<=3:
+		HISTORY.append('Can\'t hold his own in a fist fight')
+	
+	print '. '.join(HISTORY)+'.'
 
 if __name__ == '__main__':
 	create_background({})
