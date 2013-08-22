07b541b59c2ad675590b598cca07ac7be9e3a11b
flags <jetstarforever@gmail.com>
2013-08-22 16:33:14 -0400

    Working on SMP

--- smp.py
+++ smp.py
@@ -2,7 +2,7 @@ from multiprocessing import Pool
 from globals import *
 
 import alife
-import life
+import life as lfe
 
 import time
 
@@ -15,13 +15,30 @@ def test(life, key=None):
 def process(callback, life, **kwargs):
 	return [life, callback(life, **kwargs)]
 
-def fast_scan_surroundings(center_chunk_key, vision):
+def _fast_scan_return(ret):
+	print 'ret', ret
+
+def fast_scan_surroundings(center_chunk_key, chunk_size, vision):
 	#alife.sight.scan_surroundings(life, judge=False, get_chunks=True)
-	alife.sight._scan_surroundings(center_chunk_key, 4, vision)
+	return alife.sight._scan_surroundings(center_chunk_key, chunk_size, vision)
 		
 def scan_all_surroundings():
-	_center_chunks = [life.get_current_chunk_id(l) for l in LIFE.values()]
-	_chunk_size = [WORLD_INFO['chunk_size'] for l in range(len(LIFE))]
-	_vision = [alife.sight.get_vision(l) for l in LIFE.values()]
-	SETTINGS['smp'].map(fast_scan_surroundings, _center_chunks, _vision)
-	SETTINGS['smp'].join()
+	_results = []
+	for life in LIFE.values():
+		_results.append(SETTINGS['smp'].apply_async(fast_scan_surroundings,
+			                                 args=(lfe.get_current_chunk_id(life),
+			                                       WORLD_INFO['chunk_size'],
+			                                       alife.sight.get_vision(life),),
+		                                      callback=_fast_scan_return))
+	
+	while _results:
+		_rem = []
+		for result in _results:
+			if result.ready():
+				_rem.append(result)
+		
+		for r in _rem:
+			_results.remove(r)
+	
+	
+	
