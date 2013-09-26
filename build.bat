6a0ef2ccea0fcfc5ebfd75a8e0d1e32303f3fa9a
flags <jetstarforever@gmail.com>
2013-09-26 14:48:47 -0400

    Build script for Windows

---
+++ build.bat
@@ -0,0 +1,8 @@
+python freeze.py py2exe
+mkdir dist\data
+xcopy data dist\data /E
+copy libtcod-mingw.dll dist
+copy SDL.dll dist
+copy terminal.png dist
+copy license.txt dist
+copy readme.md dist\readme.txt
