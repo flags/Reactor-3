6fa4862240072d21263c9eb0b176317c5e6982c3
flags <jetstarforever@gmail.com>
2014-02-18 16:59:52 -0500

    Recoil work

--- tests/M07_recoil.py
+++ tests/M07_recoil.py
@@ -7,10 +7,10 @@ import math
 _weapon = {'recoil': 0.3,
            'accuracy': 0.9}
 stance_mod = 1.0
-aim_difficulty = 1.3
+aim_difficulty = 1.8
 firearms_skill_mod = 0.55
 hit_certainty_mod = 0.6
-bullets = 100
+bullets = 5
 simulaton_ticks = 8
 
 def load_weapon(weapon_file):
@@ -35,15 +35,13 @@ def bullet_trajectory(bullet_pos, bullet_direction, ticks=simulaton_ticks):
 	
 	return bullet_pos
 
-def simulate(firearms_skill):
-	recoil = 0.0
+def simulate(firearms_skill, recoil=0.0):
+	recoil = float(recoil)
 	bullet_direction = 0
 	bullet_pos = [0, 0]
 	bullet_velocity = [0, 0]
 	_deviations = []
 	_hits = 0
-	
-	print 'Skill:', firearms_skill
 	
 	for i in range(bullets):
 		bullet_velocity = [0, 0]
@@ -53,8 +51,10 @@ def simulate(firearms_skill):
 		deviation = (bullet_deviation*aim_difficulty)*deviation_mod
 		bullet_direction = random.uniform(-deviation, deviation)
 		
-		#print 'Shot #%s (dev_mod=%0.4f, rec=%0.3f, ticks=%s)' % (i+1, deviation_mod, recoil, simulaton_ticks)
-		recoil = clip(recoil+(_weapon['recoil']*stance_mod), 0.0, 1.0)
+		######################
+		## THIS DOES RECOIL ##
+		######################
+		#recoil = clip(recoil+(_weapon['recoil']*stance_mod), 0.0, 1.0)
 		
 		_end_pos = bullet_trajectory(bullet_pos, bullet_direction)
 		_direction_deviation = abs(bullet_direction)
@@ -62,8 +62,8 @@ def simulate(firearms_skill):
 		_hits += _target_hit_certainty<=1
 		_deviations.append(_direction_deviation)
 	
-	print 'Deviations: mean=%0.4f, min=%0.4f, max=%0.4f' % (sum(_deviations)/len(_deviations), min(_deviations), max(_deviations))
-	print 'Accuracy:', _hits/float(bullets), '\n'
+	#print 'Deviations: mean=%0.4f, min=%0.4f, max=%0.4f' % (sum(_deviations)/len(_deviations), min(_deviations), max(_deviations))
+	#print 'Accuracy:', _hits/float(bullets), '\n'
 	
 	#print 'Trajectory deviation: %0.4f' % _direction_deviation
 	#print 'Non-certainty: %s (hit=%s)' % (_target_hit_certainty, _target_hit_certainty<=1)
@@ -73,11 +73,16 @@ def simulate(firearms_skill):
 	
 	return _hits/float(bullets)
 
-_path = 'C:\\cygwin\\home\\Luke\\code\\Reactor-3\\data\\items\\glock.json'
+_path = 'C:\\cygwin\\home\\Luke\\code\\Reactor-3\\data\\items\\22_rifle.json'
 load_weapon(_path)
 
-_accuracies = []
-for i in range(1, 10+1):
-	_accuracies.append(simulate(i))
+#_accuracies = []
 	
-	print 'Total accuracy: %0.4f' % (sum(_accuracies)/float(len(_accuracies)))
+for i in range(1, 10+1):
+	print 'Skill: %s\t' % i
+	
+	for r in range(0, 5):
+		recoil = r/5.0
+		_accuracies = simulate(i, recoil=recoil)
+	
+		print '\tAccuracy=%0.4f, recoil=%0.4f' % (_accuracies, recoil)
