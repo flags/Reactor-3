231d3c1ddee7eac6abc4fd4e1808e0ea465f1193
Kyle Benesch <4b796c65+github@gmail.com>
2021-04-28 01:20:52 -0700

    Add a workaround for the old input API.
    Partial fixes for the keypad and shift key.

--- inputs.py
+++ inputs.py
@@ -21,6 +21,23 @@ def get_input():
 	get_keyboard_input()
 	get_mouse_input()
 
+VK_MAPPINGS = {
+	tcod.KEY_RIGHT: "right",
+	tcod.KEY_LEFT: "left",
+	tcod.KEY_DOWN: "down",
+	tcod.KEY_UP: "up",
+	tcod.KEY_KP0: "0",
+	tcod.KEY_KP1: "1",
+	tcod.KEY_KP2: "2",
+	tcod.KEY_KP3: "3",
+	tcod.KEY_KP4: "4",
+	tcod.KEY_KP5: "5",
+	tcod.KEY_KP6: "6",
+	tcod.KEY_KP7: "7",
+	tcod.KEY_KP8: "8",
+	tcod.KEY_KP9: "9",
+}
+
 def get_keyboard_input():
 	global KEYBOARD_STRING
 	
@@ -29,16 +46,13 @@ def get_keyboard_input():
 
 	if KEY.c:
 		_key = chr(KEY.c)
+		if KEY.shift:
+			_key = _key.upper()
+			if _key == "/":
+				_key = "?"
 	else:
-		if KEY.pressed:
-			if KEY.vk == tcod.KEY_RIGHT:
-				INPUT['right'] = True
-			elif KEY.vk == tcod.KEY_LEFT:
-				INPUT['left'] = True
-			elif KEY.vk == tcod.KEY_DOWN:
-				INPUT['down'] = True
-			elif KEY.vk == tcod.KEY_UP:
-				INPUT['up'] = True
+		if KEY.pressed and KEY.vk in VK_MAPPINGS:
+			INPUT[VK_MAPPINGS[KEY.vk]] = True
 		
 		return True
 	
